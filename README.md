50245e77adc75720c1a4ceb3f9c008b70090e10c
Vurv <56230599+Vurv78@users.noreply.github.com>
2023-06-28 18:53:10 -0700

    qjson

---
+++ README.md
@@ -0,0 +1,47 @@
+# `qjson.lua`
+
+Tiny and Rapid JSON encoding/decoding in pure Lua.
+
+(Running benchmark by [`rxi/json.lua`](https://github.com/rxi/json.lua))
+```
+Lua version   : LuaJIT 2.1.0-beta3
+CPU name      : 11th Gen Intel(R) Core(TM) i7-11800H @ 2.30GHz
+../json.lua   : 0.00494s [x7.06e+03] (min: 0.00452s, max 0.00526s)
+dkjson.lua    : 0.0121s [x1.73e+04] (min: 0.0118s, max 0.0123s)
+jfjson.lua    : 0.00993s [x1.42e+04] (min: 0.00954s, max 0.0103s)
+qjson_simple.lua  : 4.3e-06s [x6.14] (min: 0s, max 1.6e-05s)
+qjson.lua : 7e-07s [x1] (min: 0s, max 1e-06s)
+```
+
+## Features
+* Pure lua, should work on every version (5.1-5.4, JIT)
+* Incredibly fast: ~10000x faster than rxi/json at decoding.
+* Actually tiny, < 150 loc.
+* Decent error handling: `Expected : to follow key for object at char 39`
+
+## Usage
+```lua
+local json = require "qjson"
+print(json.encode {
+	hello = "world!",
+})
+
+--[[
+	{"qjson": ["fast","simple","tiny"],"hello": "world!"}
+]]
+
+print(json.decode([[
+	{ "foo": "bar" }
+]]))
+```
+
+## Notes
+* `true`, `false` and `null` are represented as strings.
+
+## qjson_simple
+
+Smaller initial version of `qjson` without all of the optimizations that make it less readable.
+
+Often runs worse than `qjson`, but can run at around the same speed.
+
+Doesn't support escaping strings, exponential numbers, or `null`.
---
+++ qjson.lua
@@ -0,0 +1,149 @@
+local find, sub, gsub, tonumber, error = string.find, string.sub, string.gsub, tonumber, error
+local function decode(json --[[@param json string]]) ---@return table
+	local ptr = 0
+	local function consume(pattern --[[@param pattern string]]) ---@return string?
+		local _, finish, match = find(json, pattern, ptr)
+		if finish then
+			ptr = finish + 1
+			return match or true
+		end
+		return nil
+	end
+
+	local repl = { -- escape characters
+		["b"] = "\b", ["f"] = "\f", ["n"] = "\n", ["r"] = "\r",
+		["t"] = "\t", ["\\"] = "\\", ["\""] = "\""
+	}
+
+	local function slowstring() -- string parser that works with escapes. fallback at very far end of parsing order, since rare case.
+		local _, start = find(json, "^%s*\"", ptr)
+		if not start then return end
+		ptr = start + 1
+
+		while true do
+			local _, finish, escapes = find(json, "(\\*)\"", ptr)
+			if finish then
+				ptr = finish + 1
+				if #escapes % 2 == 0 then
+					return ( gsub( sub(json, start + 1, finish - 1), "\\([bfnrt\\\"])", repl) )
+				end
+			else
+				error("Missing end quote for string at char " .. ptr)
+			end
+		end
+	end
+
+	local object, array
+	local function faststring() -- Parses a string without any escapes. Fastest and most common case.
+		return consume("^%s*\"([^\"\\]*)\"")
+	end
+
+	--- Parses a JSON value.
+	--- Order is important for performance with common cases.
+	local function value()
+		return faststring()
+			or object()
+			or tonumber(consume("^%s*(%-?%d+%.%d+)") or consume("^%s*(%-?%d+)"))
+			or consume("^%s*(true)") or consume("^%s*(false)") or consume("^%s*(null)")
+			or array()
+			or tonumber(consume("^%s*(%-?%d+[eE][%+%-]?%d+)")) -- Exponential number.
+			or slowstring()
+	end
+
+	function object()
+		if consume("^%s*{") then
+			local fields = {}
+			if consume("^%s*}") then return fields end
+
+			repeat
+				local key = faststring()
+				if not key then
+					error("Expected field for object at char " .. ptr)
+				end
+
+				if not consume("^%s*:") then
+					error("Expected : to follow key for object at char " .. ptr)
+				end
+
+				local val = value()
+				if val then
+					fields[key] = val
+				else
+					error("Expected value for field " .. key .. " at char " .. ptr)
+				end
+
+				consume("^%s*,")
+			until consume("^%s*}")
+
+			return fields
+		end
+	end
+
+	function array()
+		if consume("^%s*%[") then
+			local values, nvalues = {}, 0
+			if consume("^%s*%]") then return values end
+
+			repeat
+				nvalues = nvalues + 1
+				local value = value()
+				if value then
+					values[nvalues] = value
+				else
+					error("Expected value for field #" .. nvalues + 1 .. " at char " .. ptr)
+				end
+				consume("^%s*,")
+			until consume("^%s*%]")
+
+			return values
+		end
+	end
+
+	return object()
+end
+
+local concat, tostring, pairs = table.concat, tostring, pairs
+local function isarray(t)
+	local i = 1
+	for k in pairs(t) do
+		if type(k) ~= "number" or k ~= i then
+			return false
+		end
+		i = i + 1
+	end
+	return true
+end
+
+local encode
+local function value(v)
+	local t = type(v)
+	if t == "table" then
+		return encode(v)
+	elseif t == "string" then
+		return "\"" .. v .. "\""
+	else
+		return tostring(v)
+	end
+end
+
+function encode(tbl --[[@param tbl table]]) ---@return string
+	if isarray(tbl) then
+		local strs, len = {}, #tbl
+		for i = 1, len do
+			strs[i] = value(tbl[i])
+		end
+		return "[" .. concat(strs, ",", 1, len) .. "]"
+	else
+		local kvs, nkvs = {}, 0
+		for k, v in pairs(tbl) do
+			nkvs = nkvs + 1
+			kvs[nkvs] = "\"" .. tostring(k) .. "\"" .. ": " .. value(v)
+		end
+		return "{" .. concat(kvs, ",", 1, nkvs) .. "}";
+	end
+end
+
+return {
+	encode = encode,
+	decode = decode
+}
