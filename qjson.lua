356e768abae58217eeff9202e633e851e87b123d
Vurv <56230599+Vurv78@users.noreply.github.com>
2023-07-09 14:34:52 -0700

    perf: use string.byte and string.match
    string.byte is faster as it doesn't need to convert the strings to their characters to compare them.
    
    string.match is *likely* faster than string.find since it doesn't automatically return the whole string in case of no capture groups being given.

--- qjson.lua
+++ qjson.lua
@@ -1,27 +1,25 @@
 local NULL = {}
 
-local find, sub, tonumber, error = string.find, string.sub, tonumber, error
+local find, match, sub, byte, tonumber, error = string.find, string.match, string.sub, string.byte, tonumber, error
 local function decode(json --[[@param json string]]) ---@return any
 	local ptr = 1
 
-	local function consume(pattern --[[@param pattern string]]) ---@return string?
-		local _, finish, match = find(json, pattern, ptr)
+	local function skip(pattern --[[@param pattern string]]) ---@return true?
+		local finish = match(json, pattern, ptr)
 		if finish then
-			ptr = finish + 1
-			return match or true
+			ptr = finish
+			return true
 		end
 	end
 
 	local function slowstring() -- string parser that supports escapes.
-		local start = ptr + 1 -- skip initial quote
-		ptr = start
-
+		local start = ptr
 		while true do
-			local start, finish = find(json, "\\*\"", ptr)
-			if finish then
-				ptr = finish + 1
-				if (finish - start) % 2 == 0 then
-					return sub(json, start, finish - 1) -- return (gsub(sub(json, start, finish - 1), "\\([bfnrt\\\"])", ESCAPES))
+			local _start, before_quote, after_quote = match(json, "()\\*()\"()", ptr)
+			if _start then
+				ptr = after_quote
+				if (before_quote - _start) % 2 == 0 then
+					return sub(json, start, after_quote) -- return (gsub(sub(json, start, finish - 1), "\\([bfnrt\\\"])", ESCAPES))
 				end
 			else
 				error("Missing end quote for string at char " .. ptr)
@@ -31,67 +29,80 @@ local function decode(json --[[@param json string]]) ---@return any
 
 
 	local function number()
-		return tonumber(consume("^(-?%d*.?%d+[eE]?[+-]?%d*)"))
+		local number, finish = match(json, "^(-?%d*.?%d+[eE]?[+-]?%d*)()", ptr - 1)
+		if finish then
+			ptr = finish
+			return tonumber(number)
+		end
 	end
 
 	local value
 	local function whitespace()
-		ptr = find(json, "%S", ptr) or ptr -- skip past whitespace, return immediate value
+		ptr = match(json, "^%s*()", ptr) or ptr -- skip past whitespace, return immediate value
 		return value()
 	end
 
-	local function string()
-		local start = ptr + 1
-		local quot = find(json, '"', start, true)
-		local prev = quot - 1
-		if sub(json, prev, prev) ~= "\\" then
-			ptr = quot + 1
-			return sub(json, start, prev)
-		else
-			return slowstring()
-		end
-	end
-
 	local peek = {
-		["\""] = string,
-		["t"] = function()
-			if sub(json, ptr, ptr + 3) == "true" then
-				ptr = ptr + 4
+		[34 --[["]]] = function()
+			local start = ptr
+			local quot = find(json, '"', start, true)
+			local prev = quot - 1
+			if byte(json, prev) ~= 92 --[[\]] then
+				ptr = quot + 1
+				return sub(json, start, prev)
+			else
+				return slowstring()
+			end
+		end,
+		[116 --[[t]]] = function()
+			if sub(json, ptr, ptr + 2) == "rue" then
+				ptr = ptr + 3
 				return true
 			end
 		end,
-		["f"] = function()
-			if sub(json, ptr, ptr + 4) == "false" then
-				ptr = ptr + 5
+		[102 --[[f]]] = function()
+			if sub(json, ptr, ptr + 3) == "alse" then
+				ptr = ptr + 4
 				return false
 			end
 		end,
-		["n"] = function()
-			if sub(json, ptr, ptr + 3) == "null" then
-				ptr = ptr + 4
+		[110 --[[n]]] = function()
+			if sub(json, ptr, ptr + 2) == "ull" then
+				ptr = ptr + 3
 				return NULL
 			end
 		end,
 
-		["0"] = number, ["1"] = number, ["2"] = number,
-		["3"] = number, ["4"] = number, ["5"] = number,
-		["6"] = number, ["7"] = number, ["8"] = number,
-		["9"] = number, ["-"] = number,
+		[48 --[[0]]] = number, [49] = number, [50] = number,
+		[51] = number, [52] = number, [53] = number,
+		[54] = number, [55] = number, [56] = number,
+		[57 --[[9]]] = number, [45 --[[-]]] = number,
 
-		["{"] = function()
-			ptr = ptr + 1
-
+		[123 --[[{]]] = function()
 			local fields = {}
-			if consume("^%s*}") then return fields end
+			if skip("^%s*}()") then return fields end
 
 			repeat
-				ptr = find(json, "%S", ptr) or ptr -- skip whitespace inline
-				local key = string()
-				if not key then
+				ptr = match(json, "()%S", ptr) or ptr -- skip whitespace inline
+
+				if byte(json, ptr) ~= 34 --[["]] then
 					error("Expected field for object at char " .. ptr)
 				end
 
-				if not consume("^%s*:") then
+				local start = ptr + 1
+				local quot = find(json, '"', start, true)
+
+				local prev = quot - 1
+				local key
+
+				if byte(json, prev) ~= 92 --[[\]] then
+					ptr = quot + 1
+					key = sub(json, start, prev)
+				else
+					key = slowstring()
+				end
+
+				if not skip("^%s*:()") then
 					error("Expected : to follow key for object at char " .. ptr)
 				end
 
@@ -102,38 +113,38 @@ local function decode(json --[[@param json string]]) ---@return any
 					error("Expected value for field " .. key .. " at char " .. ptr)
 				end
 
-				consume("^%s*,")
-			until consume("^%s*}")
+				ptr = match(json, "^%s*,()", ptr) or ptr
+			until skip("^%s*}()")
 
 			return fields
 		end,
 
-		["["] = function()
-			ptr = ptr + 1 -- Already know we're at the [ from value()
-
+		[91 --[=[]]=]] = function()
 			local values, nvalues = {}, 0
-			if consume("^%s*%]") then return values end
+			if skip("^%s*%]()") then return values end
 
 			repeat
-				nvalues = nvalues + 1
 				local value = value()
 				if value ~= nil then
+					nvalues = nvalues + 1
 					values[nvalues] = value
 				else
-					error("Expected value for field #" .. nvalues + 1 .. " at char " .. ptr)
+					error("Expected value for field #" .. nvalues .. " at char " .. ptr)
 				end
-				consume("^%s*,")
-			until consume("^%s*%]")
+
+				ptr = match(json, "^%s*,()", ptr) or ptr
+			until skip("^%s*%]()")
 
 			return values
 		end,
 
-		[" "] = whitespace, ["\t"] = whitespace, ["\n"] = whitespace, ["\r"] = whitespace,
+		[9 --[[\t]]] = whitespace, [10 --[[\n]]] = whitespace, [13 --[[\r]]] = whitespace, [32 --[[ ]]] = whitespace,
 	}
 
 	function value()
-		local p = peek[sub(json, ptr, ptr)]
+		local p = peek[byte(json, ptr)]
 		if p then
+			ptr = ptr + 1
 			return p()
 		else
 			error("Failed parsing at character " .. ptr)
@@ -183,12 +194,11 @@ function _encode(tbl --[[@param tbl table]], buffer --[[@param buffer table]], n
 			buffer[nbuffer] = ","
 		end
 
-		if len ~= 0 then
-			buffer[nbuffer] = "]"
-		else
+		if len == 0 then -- no trailing comma to replace. need to increment ptr
 			nbuffer = nbuffer + 1
-			buffer[nbuffer] = "]"
 		end
+
+		buffer[nbuffer] = "]"
 	else
 		nbuffer = nbuffer + 1
 		buffer[nbuffer] = "{"
@@ -201,12 +211,11 @@ function _encode(tbl --[[@param tbl table]], buffer --[[@param buffer table]], n
 			buffer[nbuffer] = ","
 		end
 
-		if nbuffer ~= prev then
-			buffer[nbuffer] = "}"
-		else
+		if nbuffer == prev then -- no trailing comma to replace. need to increment ptr
 			nbuffer = nbuffer + 1
-			buffer[nbuffer] = "}"
 		end
+
+		buffer[nbuffer] = "}"
 	end
 
 	return nbuffer
